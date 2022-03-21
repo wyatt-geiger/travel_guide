e6f0a81ec5f6965b857ab5deef54037edc2256b8
BaldEagleFacts <email>
2022-03-20 22:25:25 -0500

    Yelp API now posts un-formatted entries to mapbox_map.html

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 from flask import Flask, render_template, request  # NOT the same as requests
 from location import location_main
 from YouTubeAPI import youtubeAPI_request
-## from yelpbasic import business_data
+from yelpbasic import yelp_call
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
+
 @app.route('/get_map')
 def get_mapbox_map():
+    yelpID = ""
 
     city = request.args.get('city')
-    
+
     state = request.args.get('state')
 
     country = request.args.get('country')
 
+    searchTerm = request.args.get('searchTerm')
+
     videoID = str(youtubeAPI_request(city, country))
-    
-    ## yelpID =  str(business_data)
 
+    if searchTerm != "":
+        yelpID = yelp_call(searchTerm, city, state, country)
 
-   
-
-    return render_template('mapbox_map.html', city=city, country=country, state=state, videoID=videoID)
-## , yelpID = yelpID
-
+    return render_template('mapbox_map.html', city=city, country=country, state=state, videoID=videoID, yelpID=yelpID)
--- yelpbasic.py
+++ yelpbasic.py
@@ -1,55 +1,69 @@
-
 import requests
 from pprint import pprint
 
-from key import my_key
+from key import yelp_key
 
 
+def yelp_call(search_term, city, state, country):
+    API_key = yelp_key
 
-API_key = my_key
+    url = 'https://api.yelp.com/v3/businesses/search'
 
-url = 'https://api.yelp.com/v3/businesses/search'
-
-headers = {
+    headers = {
         'Authorization': 'Bearer %s' % API_key,
     }
 
+    term = search_term
+    if state == "":
+        location = city
+    else:
+        location = city
+    radius = 16090
+    limit = 5
 
-
-term=input("Input The Term to Search Yelp : ")
-location=input("Input The Location to Search Yelp : ")
-radius =int(input("Input The Radius Value : "))
-limit = int(input("Input the Limit of restraunts: "))
-
-Parameter = {
+    Parameter = {
         'term': term,
         'location': location,
         'radius': radius,
         'limit': limit
 
-       
-        }
+    }
+
+    response = requests.get(url=url, params=Parameter, headers=headers, )
+
+    # convert the json string to dic
+    business_data = response.json()
+    business_list = yelp_interpreter(business_data)
+    return business_list
+    # print(business_data.keys()) # to see the dictionary for the of all [businnes, total, region]
 
 
-response = requests.get(url = url, params = Parameter, headers=headers, )
+def yelp_interpreter(business_data):
+    business_list = []
+    for business_names in business_data["businesses"]:
+        business_list.append([business_names['name'],
+                              business_names['rating'],
+                              business_names['location']['address1'],
+                              business_names['location']['city'],
+                              business_names['phone']])
+        # print("{:25s}{:5.2f}{:>25s}{:>25s}{:>15}{:>25}{:>15f}".format(business_names['name'],
+        #                                                               business_names['rating'],
+        #                                                               business_names['location']['address1'],
+        #                                                               business_names['location']['city'],
+        #                                                               business_names['phone'],
+        #                                                               business_names['review_count'],
+        #                                                               business_names['coordinates']['latitude']
+        #                                                               ))
+    return business_list
 
-#convert the json string to dic
-business_data = response.json()
-# print(business_data.keys()) # to see the dictionary for the of all [businnes, total, region]
-
-
-for businen_names in business_data["businesses"]:  # show only the business names 
-        # print(businen_names['name'])  #show only the business names s
-        print("{:25s}{:5.2f}{:>25s}{:>25s}{:>15}{:>25}{:>15f}".format(businen_names['name'],
-        businen_names['rating'],
-        businen_names['location']['address1'],
-        businen_names['location']['city'],
-        businen_names['phone'],
-        businen_names['review_count'],
-        businen_names['coordinates']['latitude']
-        ))
-        
-
-
-
-       
+#
+# for businen_names in business_data["businesses"]:  # show only the business names
+#     # print(businen_names['name'])  #show only the business names s
+#     print("{:25s}{:5.2f}{:>25s}{:>25s}{:>15}{:>25}{:>15f}".format(business_names['name'],
+#                                                                   business_names['rating'],
+#                                                                   business_names['location']['address1'],
+#                                                                   business_names['location']['city'],
+#                                                                   business_names['phone'],
+#                                                                   business_names['review_count'],
+#                                                                   business_names['coordinates']['latitude']
+#                                                                   ))
