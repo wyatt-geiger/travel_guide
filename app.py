502acc0052975fe16bcd07b1f7e7acc3b2bc6657
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-03-24 01:35:03 -0500

    Cache feature implemented and finished using Flask-Caching extension. Ready to merge

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request  # NOT the same as requests
 from location import location_main
 from YouTubeAPI import youtubeAPI_request
 from yelpbasic import yelp_call
+from flask_caching import Cache
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 
 app = Flask(__name__)
 
+app.config.from_mapping(config)
+cache = Cache(app)
 
 @app.route('/')
 def homepage():
@@ -12,6 +21,7 @@ def homepage():
 
 
 @app.route('/get_map')
+@cache.cached(timeout=50)
 def get_mapbox_map():
     yelpID = ""
 
