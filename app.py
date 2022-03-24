ac9ad3059ef3ed19aae575396daa8d3b6e89880a
clara <10088152+claraj@users.noreply.github.com>
2022-03-23 20:43:14 -0500

    delete button

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from tabnanny import check
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from location import location_main
 from YouTubeAPI import youtubeAPI_request
 from yelpbasic import yelp_call
@@ -33,15 +33,18 @@ def get_mapbox_map():
         
     return render_template('mapbox_map.html', city=city, country=country, state=state, videoID=videoID, yelpID=yelpID)
   
+
 @app.route('/get_bookmarks', methods=['GET', 'POST'])
 def bookmark_page():
 
     if request.method == 'POST':
-        name = request.form.getlist('check')
+        name = request.form.get('name')
         print(name)
+        bookmark_schema.delete_data(name)
+        return redirect('/get_bookmarks')   # reload the bookmarks page, by default a get request. now without the deleted item
+        # todo error handling 
 
-    #bookmark_schema.delete_data(name)
+    else:
 
-    bookmark_data = bookmark_schema.display_all_data()
-
-    return render_template('bookmark.html', bookmark_data=bookmark_data)
+        bookmark_data = bookmark_schema.display_all_data()
+        return render_template('bookmark.html', bookmark_data=bookmark_data)
