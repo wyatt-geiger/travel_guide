cf7d7fad3dac882ac30058a4bab4c52c735f1aed
BaldEagleFacts <email>
2022-03-28 02:38:58 -0500

    User inputs and API results are now saved to cached_data.csv, because csv was way easier to work with than json. Cached data is used instead of API requests when user search terms match an existing entry in the cache.
    Next task is to delete old entries, and do unit testing on cache

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from tabnanny import check
+import csv
+import ast
 from flask import Flask, render_template, request, redirect
-from location import location_main
 from YouTubeAPI import youtubeAPI_request
 from yelpbasic import yelp_call
 import bookmark_schema
@@ -36,18 +36,49 @@ def get_mapbox_map():
 
     searchTerm = request.args.get('searchTerm')
 
-    videoID = str(youtubeAPI_request(city, state, country))
+    try:  # This will test to see if a file containing cache data already exists
+        cache_data = open('cache_data.csv')
+        cache_reader = csv.reader(cache_data)
+        print("cache opened")
+        for row in cache_reader:
+            # this loop tests if the city, state, and country inputted by the user match any entries in the cache
+            if city + state + country == row[0] + row[1] + row[2]:
+                videoID = row[4]    # If a match is found, the youtube video from the cache is saved
 
-    if searchTerm != "":
+                if searchTerm == row[3]:  # if the search term also matches, then the cached yelp data is also saved
+                    yelpID = ast.literal_eval(row[5])
+                    # learned of ast from here: https://www.askpython.com/python/string/python-convert-string-to-list
+                    # The yelp data is saved as a list, within a csv file.
+                    # This is weird, and requires a weird solution (This ast thing) to make the list readable again.
+
+        cache_data.close()
+    except FileNotFoundError:
+        print("File not found")
+        # If the csv file doesn't exist, it will be made later
+
+    try:  # code idea from https://www.oreilly.com/library/view/python-cookbook/0596001673/ch17s02.html
+        videoID
+    except NameError:
+        videoID = None
+    if videoID is None:
+        videoID = str(youtubeAPI_request(city, state, country))
+        print("YoutubeAPI sent a request")
+    # If the csv file doesn't exist, or there isn't cached data for this search, then videoID won't exist.
+    # This tests if videoID was saved from cache or not. If not, is calls the API to make videoID.
+    # The same process happens below with the yelpID
+    try:
+        yelpID
+    except NameError:
+        yelpID = None
+    if yelpID is None and searchTerm != "":
         yelpID = yelp_call(searchTerm, city, state, country)
-    else:
-        yelpID = ""
+        print("Yelp API sent a request")
 
-    page_data = {'city': city, 'state': state, 'country': country,
-                 'search': searchTerm, 'videoID': videoID, 'businesses': yelpID, 'created': datetime.now()}
-
-    cached_data = open('cached_data.json', 'a')
-    cached_data.write(str(page_data))
+    # Below handles the creation/modification of the cache csv file.
+    cache_data = open('cache_data.csv', 'a', newline='')
+    cache_writer = csv.writer(cache_data)
+    cache_writer.writerow([city, state, country, searchTerm, videoID, yelpID, datetime.now()])
+    cache_data.close()
 
     return render_template('mapbox_map.html', city=city, country=country, state=state, yelpID=yelpID,
                            searchTerm=searchTerm, map_box_key=map_box_key, videoID=videoID)
