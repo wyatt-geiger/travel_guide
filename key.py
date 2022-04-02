9f0449b17a4130fa58417b5163b1c6507cf0ff80
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-04-02 05:01:45 -0500

    Uploading project to a new personal repository. Key.py is also now ignored by .gitignore

--- key.py
+++ key.py
@@ -1,3 +1,7 @@
-youtube_key =''
-yelp_key = ''
-map_box_key = ''
+# These keys may be stale, so ensure that they are still working
+
+youtube_key = 'AIzaSyBwZy7lTNsUiWK1z8_6vZXbbOL_CExCO6k'
+
+yelp_key = 'CYk10ERN6_7E5KSiQfa9cgNRi1I199LEmEmTYjo5qH1XC7neixnpZ5Avj0aWu6QVCNUVXmHIX010HkQ3K8uosfwFw9XlM6RMxslxTB4Xn57Y9uLsiXCUarByYrEXYnYx'
+
+map_box_key = 'pk.eyJ1IjoibWFuZGlibGVjbGF3IiwiYSI6ImNrNmxpcWJ4MzBhamozZXBiZzVoNm11cmgifQ.lt7or9puZArJpANdrnIrUg'
